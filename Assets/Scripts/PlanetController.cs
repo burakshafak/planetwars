2d40c6280a0d5401bd3eab639cc9b4271c639fe6
burak <burakshafak@hotmail.com>
2024-10-12 19:55:22 +0300

    Fireolmadı

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlanetController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1;
+    
+
     void Start()
     {
         
@@ -18,10 +20,22 @@ public class PlanetController : MonoBehaviour
         //get the Input from Vertical axis
         float verticalInput = Input.GetAxis("Player1 Vertical");
 
+       
+
         //update the position
         transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
 
-        //output to log the position change
-        Debug.Log(transform.position);
+        
+    }
+
+   
+
+
+
+    void Fire()
+    {
+       
+
+
     }
 }

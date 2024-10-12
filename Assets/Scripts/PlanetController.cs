84c16ac864da719953be6c2d79abf82ed07f4866
niranu <104153776+niranu@users.noreply.github.com>
2024-10-12 20:30:35 +0300

    bullets fired

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlanetController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1;
-    
+
+    public GameObject bulletPrefab;
+    [SerializeField] public float bulletSpeed = 1;
+
+    public GameObject planet1;
 
     void Start()
     {
@@ -20,22 +24,21 @@ public class PlanetController : MonoBehaviour
         //get the Input from Vertical axis
         float verticalInput = Input.GetAxis("Player1 Vertical");
 
-       
-
         //update the position
         transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
 
+        float fireInput = Input.GetAxis("Fire1");
+        if(fireInput != 0)
+        {
+            Fire();
+        }
         
     }
 
-   
-
-
-
     void Fire()
     {
-       
-
-
+        print("Inside fire method.");
+        var bullet = Instantiate(bulletPrefab, planet1.transform.position, planet1.transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = planet1.transform.up * bulletSpeed;
     }
 }
